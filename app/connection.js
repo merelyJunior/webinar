53e09557b35e0a4056b91900cd27d2484cd83829
vovaFrontEnd <vladimirsolenyi25@gmail.com>
2024-08-22 21:47:51 +0300

    debugging versel JSON part 7

--- app/connection.js
+++ app/connection.js
@@ -3,9 +3,9 @@ import mysql from 'mysql2/promise';
 // Настройте пул соединений
 const pool = mysql.createPool({
   host: 'localhost',
-  user: 'ch87e37f20_test',
-  password: '1q2w3e4r',
-  database: 'ch87e37f20_webinar',
+  user: 'root',
+  password: '',
+  database: 'webinar',
   timezone: 'local'
 });
 
