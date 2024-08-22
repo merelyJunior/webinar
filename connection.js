9d327848cf47d4bba781f223d3bf32b1dcbd8e3e
vovaFrontEnd <vladimirsolenyi25@gmail.com>
2024-08-22 19:01:50 +0300

    first push debugging 1

--- connection.js
+++ connection.js
@@ -2,7 +2,7 @@ import mysql from 'mysql2/promise';
 
 // Настройте пул соединений
 const pool = mysql.createPool({
-  host: 'https://85.209.154.162',
+  host: '85.209.154.162',
   user: 'root',
   password: 'Haw4RYArvXY7bLnE3psK',
   database: 'webinar',
