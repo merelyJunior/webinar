49489fa068bb54e96438cf96c0b9875fdabce5bd
vovaFrontEnd <vladimirsolenyi25@gmail.com>
2024-08-23 14:58:47 +0300

    debugging PsgSQL

--- app/connection.js
+++ app/connection.js
@@ -1,12 +1,26 @@
-import mysql from 'mysql2/promise';
+// import mysql from 'mysql2/promise';
 
-// Настройте пул соединений
-const pool = mysql.createPool({
-  host: '85.209.154.162',
-  user: 'root',
-  password: 'Haw4RYArvXY7bLnE3psK',
-  database: 'webinar',
-  timezone: 'local'
+// // Настройте пул соединений
+// const pool = mysql.createPool({
+//   host: '85.209.154.162',
+//   user: 'root',
+//   password: 'Haw4RYArvXY7bLnE3psK',
+//   database: 'webinar',
+//   timezone: 'local'
+// });
+
+// export default pool;
+import { Pool } from 'pg';
+
+// Строка подключения
+const connectionString = "postgres://default:6MtWFzg3OGEr@ep-floral-cloud-a46xmgzv.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require";
+
+// Создание пула подключений
+const pool = new Pool({
+  connectionString,
+  ssl: {
+    rejectUnauthorized: false, 
+  },
 });
 
 export default pool;
